574a65175fab277bafdc09ffcc98a5fb34b4ae9d
Adeonthebeat <fowsdw@naver.com>
2021-04-12 21:29:24 +0900

    Add files via upload

---
+++ practice/practice.py
@@ -0,0 +1,17 @@
+import sys
+import string
+import numpy
+import textwrap
+
+###########################################################
+# 자동 코드 정렬 ctrl +alt + i
+
+def print_formatted(number):
+
+    ret = ''
+
+    return ret
+
+if __name__ == "__main__":
+    n = 17
+    print_formatted(n)
