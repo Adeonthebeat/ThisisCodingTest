604bcd8bcd91129155a8e1f5b7c770a859646dff
ade <fowsdw@naver.com>
2021-05-01 23:33:04 +0900

    ade commit

--- practice/08. Shortest Distance.py
+++ practice/08. Shortest Distance.py
@@ -25,7 +25,21 @@ import sys
 # 큐(queue)  : 가장 먼저 삽입된 데이터를 추출
 # 우선순위 큐(priority_queue): 가장 우선순위가 높은 데이터를 추출
 
+# 다익스트라(HEAP)
+# 사용시기 : 한지점에서 다른 특정지점까지 최단경로를 구하는 경우
+# 핵심원리 : 방문하지 않은 노드 중 최단거리를 갖는 노드를 찾음
+# 리스트 - 1차원
+# 분류 : 그리디(Greedy)
+# 시간복잡도 : O(ElogV)
 
+# 플로이드워셜
+# 사용시기 : 모든 지점에서 다른 모든 지점까지의 최단경로를 구하는 경우
+# 핵심원리 : 방문하지 않는 노드 중 최단거리를 갖는 노드를 찾지 않음
+# 리스트 - 2차원
+# 분류 : 다이나믹프로그래밍(DP)
+# 시간복잡도 : O(N^3)
+# Dab = Distance A to B
+# 점화식 : Dab = min(Dab, Dak + Dkb)
 
 '''
 6 11
@@ -42,8 +56,6 @@ import sys
 5 3 1
 5 6 2
 '''
-
-
 
 # 간단한 다익스트라 최단경로 알고리즘
 # 시간복잡도 : O(V2) | V = 노드의 개수 
@@ -160,8 +172,187 @@ def Dijkstras_heap():
             print("INFINITY")
         else:
             print(distance[i])
+'''
+4
+7
+1 2 4
+1 4 6
+2 1 3
+2 3 7
+3 1 5
+3 4 4
+4 3 2
+'''
+# 플로이드워셜 알고리즘
+def Floyd_Warshall():
 
+    # 무한 값 세팅
+    INF = int(1e9)
+
+    # 노드의 개수와 간선의 개수 세팅
+    n = int(input())
+    m = int(input())
+
+    # 2차원리스트(그래프)를 만들고, 모든 값을 무한으로 초기화
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+    # 자기 자신으로 가는 비용은 0으로 초기화
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            if a == b:
+                graph[a][b] = 0
+
+    # 각 간선에 대한 정보를 입력받아 그 값으로 초기화
+    for _ in range(m):
+        # A에서 B로 가는 비용은 C라고 설정
+        a, b, c = map(int, input().split())
+        graph[a][b] = c
+
+    # 점화식에 따라 플로이드 워셜 알고리즘 수행
+    for k in range(1, n+1):
+        for a in range(1, n+1):
+            for b in range(1, n+1):
+                # min(a -> b, a -> x -> b)
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+    # 수행된 결과 출력
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+            if graph[a][b] == 1e9:
+                print("INFINITY", end=" ")
+            # 도달할 수 있는 경우 거리를 출력
+            else:
+                print(graph[a][b], end=" ")
+        print()
+
+'''
+5 7
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5
+
+4 2
+1 3
+2 4
+3 4
+'''
+# 문제요약 : 1번회사에서 출발해 X를 거쳐 K로 가는 최단거리를 구하라.
+def future_city():
+
+    INF = int(1e9)
+
+    # 노드, 간선의 개수 체크
+    n, m = map(int, input().split())
+    # 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+    # 자기 자신으로 가는 비용은 0으로 초기화
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if a == b:
+                graph[a][b] = 0
+
+    # 각 간선에 대한 정보를 입력받아 그 값으로 초기화
+    for _ in range(m):
+        a, b = map(int, input().split())
+        # 서로에게 가는 비용은 1 * 주의
+        graph[a][b] = 1
+        graph[b][a] = 1
+
+    # 거쳐갈 노드 X와 최종 목적지 노드 K 입력
+    x, k = map(int, input().split())
+
+    # 점화식에 따라 플로이드 워셜 알고리즘 수행
+    for k in range(1, n + 1):
+        for a in range(1, n + 1):
+            for b in range(1, n + 1):
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+    distance = graph[1][k] + graph[k][x]
+
+    if distance >= INF:
+        print("-1")
+    else:
+        print(distance)
+
+
+'''
+3 2 1
+1 2 4
+1 3 2
+'''
+# 전보문제
+# 도시 C에서 보낸 메시지를 받게 되는 도시의 개수는 총 몇개이며,
+# 도시들이 모두 메시지를 받는데까지 걸린 시간은 얼마인지 계산하시오.
+def telegram():
+
+    INF = int(1e9)
+    input = sys.stdin.readline
+
+    n, m, start = map(int, input().split())
+
+    graph = [[] for i in range(n+1)]
+    distance = [INF] * (n+1)
+
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a].append((b, c))
+
+    def dijkstras(start):
+        q = []
+        # 시작노드로 가기 위한 최단경로는 0으로 설정하여 큐에 삽입
+        heapq.heappush(q, (0, start))
+        distance[start] = 0
+
+        # 큐가 비어있지 않다면.
+        while q:
+            dist, now = heapq.heappop(q)
+            # 이미 처리된 적이 있는 노드라면, 무시
+            if distance[now] < dist:
+                continue
+
+            # 현재 노드와 연결된 인접한 노드 확인
+            for i in graph[now]:
+                # i[0] = b의 노드 | i[1] = c의 비용
+                cost = dist + i[1]
+                # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우, HEAP에서 POP
+                if cost < distance[i[0]]:
+                    distance[i[0]] = cost
+                    heapq.heappush(q, (cost, i[0]))
+
+    dijkstras(start)
+
+    # 도달할 수 있는 노드 개수
+    cnt = 0
+
+    # 도달할 수 있는 노드 중 가장 멀리 있는 노드의 거리
+    max_distance = 0
+
+    for d in distance:
+        # 도달할 수 있는 노드인 경우
+        if d != INF:
+            cnt += 1
+            max_distance = max(max_distance, d)
+
+    print(cnt-1, max_distance)
+
+
+
+
+
+
+
+    return ""
 
 if __name__ == "__main__":
     # Dijkstras()
-    Dijkstras_heap()
+    # Dijkstras_heap()
+    # Floyd_Warshall()
+    # future_city()
+    telegram()
