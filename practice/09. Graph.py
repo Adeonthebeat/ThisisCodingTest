5ac5ff0b1cd72978ea6e8789404322b94c0c55fd
ade <fowsdw@naver.com>
2021-05-06 07:30:51 +0900

    ade commit

--- practice/09. Graph.py
+++ practice/09. Graph.py
@@ -59,6 +59,20 @@
 # 시간 복잡도 : 간선의 개수(E)일 때, O(ElogE)
 # 서로소 집합 알고리즘 시간복잡도 < 정렬 시간복잡도
 
+# 위상정렬(Topology Algorithm)
+# 순서가 정해져 있는 일련의 작업을 차례대로 수행해야 할 때, 사용할 수 있는 알고리즘
+# 방향 그래프의 모든 노드를 방향성에 거스르지 않도록 순서대로 나열하는 것
+# 1. 진입차수가 0인 노드를 큐에 넣는다.
+# 2. 큐가 빌 때까지 다음의 과정을 반복한다.
+# 큐에서 원소를 꺼내 해당 노드에서 출발하는 간선을 그래프에서 제거한다.
+# 새로게 진입차수가 0이 된 노드를 큐에 넣는다.
+# 모든 원소를 방문하기 전, 큐가 비어있다면 사이클이 존재하는 것
+# 큐에서 원소가 V번 추출되기 전에 큐가 비어버리면 사이클이 발생한 것.
+# 시간복잡도: O(V + E)
+# 모든 노드를 확인하면서 해당 노드에서 출발하는 간선을 차례대로 제거
+# 위상정렬은 DAG(direct_acyclic_graph): 순환하지 않는 방향그래프의 특징
+# 정답이 1가지 이상인 경우도 있다. 위상정렬을 구현할때 stack을 이용한 DFS로도 구현할 수 있다.
+
 '''
 6 4
 1 4
@@ -68,6 +82,8 @@
 '''
 
 # 특정 원소가 속한 집합을 찾기
+from collections import deque
+
 '''
 def find_parent(parent, x):
     # 루트노드가 아니라면, 루트 노드를 찾을 때까지 재귀적 호출
@@ -200,7 +216,69 @@ def Kruskal_Algorithm():
             result += cost
     print(result)
 
+
+'''
+7 8
+1 2
+1 5
+2 3
+2 6
+3 4
+4 7
+5 6
+6 4
+'''
+
+def topology():
+    # 노드, 간선 개수 입력
+    v, e = map(int, input().split())
+
+    # 모든 노드에 대한 진입 차수는 0으로 초기화
+    indegree = [0] * (v+1)
+
+    # 각 노드에 연결된 간선 정보를 담기 위한 연결리스트 초기화
+    graph = [[] for i in range(v+1)]
+
+    # 방향그래프의 모든 간선 정보 입력받기
+    for _ in range(e):
+        a, b = map(int, input().split())
+
+        # 정점 A에서 B로 이동가능
+        graph[a].append(b)
+
+        # 진입차수를 1증가
+        indegree[b] += 1
+
+    def topology_sort():
+        result = [] # 알고리즘 수행결과를 담을 리스트
+        q = deque() # 큐 기능을 위한 deque 라이브러리
+
+        # 처음 시작할 때, 진입차수가 0인 노드를 큐에 삽입
+        for i in range(1, v+1):
+            if indegree[i] == 0:
+                q.append(i)
+
+        # 큐가 빌때까지
+        while q:
+            # 큐에서 원소 꺼내기
+            now = q.popleft()
+            result.append(now)
+
+            # 해당원소와 연결된 노드들의 진입차수에서 1빼기
+            for i in graph[now]:
+                indegree[i] -= 1
+                # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+                if indegree[i] == 0:
+                    q.append(i)
+        # 위상정렬 결과 출력
+        for i in result:
+            print(i, end=' ')
+    topology_sort()
+
+
+
 if __name__ == "__main__":
     # disjoint_sets()
     # disjoint_sets_cycle()
-    Kruskal_Algorithm()
+    # Kruskal_Algorithm()
+    topology()
