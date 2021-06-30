ebce2d9dbffdba49f66841e2d2c12aba96933394
ade <fowsdw@naver.com>
2021-06-30 23:54:02 +0900

    ade commit

--- practice/14. Binary_Search_Problem.py
+++ practice/14. Binary_Search_Problem.py
@@ -282,8 +282,58 @@ def wifi():
 
     print(ret)
 
+'''
+# 가사 검색 문제
+ - Query가 주어질 때, 각 키워드별 매치된 단어가 몇 개인지 순서대로 배열에 담아 return
+
+# Developer`s Kick!
+ - fro?? = 5자리
+ - 이진 탐색을 이용해서 'fro'로 시작하는 마지막 단어의 위치를 찾고 
+   'fro'로 시작되는 첫 단어의 위치를 찾아서 그 차이를 계산.
+ - froaa ~ frozz까지 구해보면 됨
+ - 와일드카드가 앞에 있다면? -> 역배열 단어리스트를 이진탐색을 구현 ( reversed_array )
+'''
+
+# 값이 (left_value, right_value)인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+
+    return right_index - left_index
 
 
+def lyrics():
+
+    # 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+    array = [[] for _ in range(10001)]
+
+    # 모든 단어를 길이마다 나누어서 뒤집어 저장하기 위한 리스트
+    reversed_array = [[] for _ in range(10001)]
+
+    def solution(words, queries):
+        answer = []
+
+        # 모든 단어를 접미사 와일드카드 배열, 접두사 와일드 카드 배열에 각각 삽입
+        for word in words:
+            # 단어를 삽입
+            array[len(word)].append(word)
+            # 단어를 뒤집어서 삽입
+            reversed_array[len(word)].append(word[::-1])
+
+        # 이진 탐색을 수행하기 위한 각 단어 리스트 정렬 수행
+        for i in range(10001):
+            array[i].sort()
+            reversed_array[i].sort()
+
+        # 쿼리를 하나씩 확인하며 처리
+        for q in queries:
+            # 접미사에 와일드 카드가 붙은 경우
+            if q[0] != '?':
+                ret = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+            else:
+                ret = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+            answer.append(ret)
+        return answer
 
 if __name__ == "__main__":
     # solution()
