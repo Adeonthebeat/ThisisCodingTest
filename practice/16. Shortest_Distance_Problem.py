fb62186054b013ef4df9b1105d6926f18367282b
ade <fowsdw@naver.com>
2021-07-21 22:30:45 +0900

    ade commit

--- practice/16. Shortest_Distance_Problem.py
+++ practice/16. Shortest_Distance_Problem.py
@@ -18,9 +18,84 @@
                 abj[a][b] = min(abj[a][b], abj[k][a] + abj[k][b])
 '''
 
+'''
+# 플로이드 문제 (모든 정점에서 모든 정점으로의 최단 경로)
+ - n개의 도시(100개 이하)
+ - m개의 버스(100,000)
+ - 시작도시 A, 도착도시 B, 필요한 비용 C
+ - A -> B로 가는데 필요한 최소값
+# Developer`s Kick!
+ - A -> B 연결하는 간선이 여러 개일수 있음
+ - 각각 비용 중 가장 짧은 간선 선택.
 
+Input01
+5
+14
+1 2 2
+1 3 3
+1 4 1
+1 5 10
+2 4 2
+3 4 1
+3 5 1
+4 5 3
+3 5 10
+3 1 8
+1 4 2
+5 1 7
+3 4 2
+5 2 4
 
+OutPut01
+0 2 3 1 4
+12 0 15 2 5
+8 5 0 1 1
+10 7 13 0 3
+7 4 10 6 0
 
+'''
+def floyd():
+    
+    # 무한
+    INF = 1e9
+    
+    # 노드의 개수와 간선의 개수를 입력받기
+    n = int(input())
+    m = int(input())
+
+    # 2차원 리스트(그래프)를 만들고 모든 값을 무한으로 초기화
+    graph = [[INF] * (n+1) for _ in range(n+1)]
+
+    # 자기 자신에서 자신으로 가는 비용은 0으로 초기화
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if a == b:
+                graph[a][b] = 0
+
+    # 각 간선의 정보(버스)를 입력 받아 그 값으로 초기화
+    for _ in range(m):
+
+        # A에서 B로 가는 C의 비용
+        a, b, c = map(int, input().split())
+
+        # 가장 짧은 간선 정보 저장
+        if c < graph[a][b]:
+            graph[a][b] = c
+
+    # 점화식에 따라 플로이드 워셜 알고리즘 수행
+    for i in range(1, n+1):
+        for a in range(1, n+1):
+            for b in range(1, n+1):
+                graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b])
+
+    # 결과물 출력
+    for a in range(1, n+1):
+        for b in range(1, n + 1):
+            if graph[a][b] == INF:
+                print(0, end=" ")
+            else:
+                print(graph[a][b], end=" ")
+        print()
 
 
 
@@ -29,3 +104,4 @@
 
 
 if __name__ == "__main__":
+    floyd()
