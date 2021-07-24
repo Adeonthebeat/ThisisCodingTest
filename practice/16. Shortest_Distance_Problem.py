e558f33c8cee20230b15ef25e654dcdd6c188ef4
ade <fowsdw@naver.com>
2021-07-24 21:49:02 +0900

    ade commit

--- practice/16. Shortest_Distance_Problem.py
+++ practice/16. Shortest_Distance_Problem.py
@@ -18,6 +18,9 @@
                 abj[a][b] = min(abj[a][b], abj[k][a] + abj[k][b])
 '''
 
+import sys
+import heapq
+
 '''
 # 플로이드 문제 (모든 정점에서 모든 정점으로의 최단 경로)
  - n개의 도시(100개 이하)
@@ -163,6 +166,103 @@ def rank():
     print(ret)
 
 
+'''
+# 화성 탐사 문제
+ - N X N 크기의 2차원 공간을 지나가며 각각의 칸을 지나가기 위한 비용(에너지 소모량)이 존재
+ - 출발지점([0][0])부터 도착지점([N-1][N-1])까지의 최소비용을 구하시오.
+# Developer`s Kick!
+ - A -> B 비용은 B 위치의 탐사비용 || B -> A 비용은 A 위치의 탐사비용
+ - 다익스트라 최단경로 알고리즘
+ 
+Input01
+3
+3
+5 5 4
+3 9 1
+3 2 7
+5
+3 7 2 0 1
+2 8 0 9 1
+1 2 1 8 1
+9 8 9 2 0
+3 6 5 1 5
+7
+9 0 5 1 1 5 3
+4 1 2 1 6 5 3
+0 7 6 1 6 8 5
+1 1 7 8 3 2 3
+9 4 0 7 6 4 1
+5 8 3 2 4 8 3
+7 4 8 4 8 3 4
+
+Output01 
+20
+19
+36
+
+'''
+def mars():
+
+    input = sys.stdin.readline
+
+    # 무한 값
+    INF = 1e9
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    for tc in range(int(input())):
+
+        # 노드의 개수 입력 받기
+        n = int(input())
+
+        # 전체 맵 정보 입력받기
+        graph = []
+        for i in range(n):
+            graph.append(list(map(int, input().split())))
+
+        # 최단거리 테이블을 모두 무한으로 초기화
+        distance = [[INF] * n for _ in range(n)]
+
+        # 시작위치 [0, 0]
+        x, y = 0, 0
+
+        # 시작노드로 가기 위한 비용은 (0, 0) 위치의 값을 설정하여 큐에 삽입
+        q = [(graph[x][y], x, y)]
+        distance[x][y] = graph[x][y]
+
+        # 다익스트라 알고리즘 수행
+        while q:
+            # 가장 최단거리가 짧은 노드에 대한 정보 꺼내기
+            dist, x, y = heapq.heappop(q)
+
+            # 현재 노드가 이미 처리된 거라면, 노드 무시
+            if distance[x][y] < dist:
+                continue
+
+            # 현재 노드와 연결된 다른 인접한 노드 확인
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                # 맵의 범위를 벗어나는 경우, 무시
+                if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                    continue
+                cost = dist + graph[nx][ny]
+
+                # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+                if cost < distance[nx][ny]:
+                    distance[nx][ny] = cost
+                    heapq.heappush(q, (cost, nx, ny))
+
+        print(distance[n-1][n-1])
+
+                # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+
+
+
+
 if __name__ == "__main__":
     # floyd()
-    rank()
+    # rank()
+    mars()
