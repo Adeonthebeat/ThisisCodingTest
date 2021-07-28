345020397b29a9545b69e40dc0414cd513442c39
ade <fowsdw@naver.com>
2021-07-28 22:37:02 +0900

    ade commit

--- practice/17. Graph_Problem.py
+++ practice/17. Graph_Problem.py
@@ -24,6 +24,7 @@
   위상 정렬 알고리즘은 난이도가 높은 후반부 문제에서 주로 출제 됨
 
 '''
+from collections import deque
 
 '''
 # 여행계획 문제
@@ -228,6 +229,7 @@ def dark_road():
 
     print(tot - ret)
 
+
 '''
 # 행성 터널 문제
  - 행성은 3차원의 좌표 한점
@@ -255,8 +257,8 @@ Output01
 
 '''
 
-def turnel():
 
+def turnel():
     # 노드의 개수 입력 받기
     n = int(input())
 
@@ -268,7 +270,7 @@ def turnel():
     ret = 0
 
     # 부모 테이블에서 부모를 자기 자신으로 초기화
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         parent[i] = i
 
     x = []
@@ -289,9 +291,9 @@ def turnel():
     # 인접한 노드로부터 간선정보를 추출하여 처리
     for i in range(n - 1):
         # 비용 순으로 정렬하기 위해서 첫 번째 원소를 비용으로 설정
-        edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
-        edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
-        edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
+        edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+        edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+        edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
 
     # 간선을 비용 순으로 정렬
     edges.sort()
@@ -308,10 +310,151 @@ def turnel():
     print(ret)
 
 
+'''
+# 최종 순위 문제
+ - 최종 순위를 발표하는 대신 작년에 비해 순위가 바뀐 팀의 목록만 발표
+ - 올해 순위를 만드는 프로그램을 작성하시오.
+ - [체크] 
+  - 확실한 올해 순위를 만들 수 없는 경우
+  - 일관성이 없는 잘못된 정보 
+ - 첫 번째 줄 - 테스트 케이스 개수 
+ - 각 테스트 케이스
+  - 팀의 수(n)을 포함
+  - n개의 정수 ti를 포함하는 한줄 -> i : 작년에 i등을 한 팀의 번호
+  - 상대적인 등수가 바뀐 쌍의 수 : m
+  - 두 정수 ai와 b를 포함하는 m줄 -> 상대적인 등수가 바뀐 두 팀이 주어짐
+ - 확실한 순위를 찾을 수 없다면 : '?'
+ - 데이터에 일관성이 없다면, "IMPOSSIBLE"
+# Developer`s Kick!
+ - 정해진 우선순위에 맞게 전체 팀의 순서를 나열한다 ->> '위상 정렬 알고리즘'
+ - 팀 간의 순위정보를 그래프 정보로 표현한 뒤, 위상 정렬 알고리즘으로 해결
+  - '자기보다 낮은 등수(1등)를 가진 팀을 가리키기' 방향 그래프를 만든다.
+   - 이와 같이 변경된 등수를 적용하면 2가지 특이 케이스가 생김
+    1) 사이클이 발생하는 경우
+    2) 위상 정렬의 결과가 1개가 아닌 여러가지일 경우
+   - 위의 특이 케이스를 제외한다면, 오직 하나의 경우만 존재
+ - 특이 케이스를 확인하기 위해서 
+ ->> 위상정렬 수행과정에서 큐에서 노드를 뽑을 때, 큐의 원소가 항상 1개로 유지되는 경우에만 고유한 순위가 존재
+
+Input01
+3
+5
+5 4 3 2 1
+2
+2 4
+3 4
+3
+2 3 1
+0
+4
+1 2 3 4
+3
+1 2
+3 4
+2 3
+
+Output01 
+5 3 2 4 1
+2 3 1
+IMPOSSIBLE
+
+'''
+
+
+def final_rank():
+    # 테스트 케이스 만큼 반복
+    for tc in range(int(input())):
+
+        # 노드의 개수 입력받기(팀의 수)
+        n = int(input())
+
+        # 모든 노드에 대한 진입차수는 0으로 초기화
+        indegree = [0] * (n + 1)
+
+        # 각 노드에 연결된 간선 정보를 담기 위한 인접행렬 초기화
+        graph = [[False] * (n + 1) for _ in range(n + 1)]
+
+        # 작년 순위 정보 입력
+        data = list(map(int, input().split()))
+
+        # 방향 그래프의 간선 정보 초기화
+        for i in range(n):
+            for j in range(i + 1, n):
+                graph[data[i]][data[j]] = True
+                indegree[data[j]] += 1
+
+        # 올해 순위 정보 입력
+        m = int(input())
+        for i in range(m):
+
+            a, b = map(int, input().split())
+
+            # 간선의 방향 뒤집기
+            if graph[a][b]:
+                graph[a][b] = False
+                graph[b][a] = True
+                indegree[a] += 1
+                indegree[b] -= 1
+            else:
+                graph[a][b] = True
+                graph[b][a] = False
+                indegree[a] -= 1
+                indegree[b] += 1
+
+        # 위상 정렬(Topology Sort) 시작
+        ret = []  # 알고리즘 수행 결과를 담을 리스트
+        q = deque()  # 큐 기능을 위한 deque 라이브러리 사용
+
+        # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
+        for i in range(1, n + 1):
+            if indegree[i] == 0:
+                q.append(i)
+
+        certain = True  # 위상 정렬 결과가 오직 하나인지의 여부
+        cycle = False  # 그래프 내 사이클이 존재하는지 여부
+
+        # 정확히 노드의 개수만큼 반복
+        for i in range(n):
+            # 큐가 비어 있다면, 사이클 발생
+            if len(q) == 0:
+                cycle = True
+                break
+            # 큐의 원소가 2개 이상이라면, 가능한 정렬결과가 여러 개라는 의미
+            if len(q) > 2:
+                certain = False
+                break
+
+            # 큐에서 원소 꺼내기
+            now = q.popleft()
+            ret.append(now)
+
+            # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
+            for j in range(1, n + 1):
+                if graph[now][j]:
+                    indegree[j] -= 1
+
+                    # 새롭게 진입차수가 0인 되는 노드를 큐에 삽입
+                    if indegree[j] == 0:
+                        q.append(j)
+
+        # 사이클이 발생하는 경우(일관성이 없는 경우)
+        if cycle:
+            print("IMPOSSIBLE")
+
+        # 위상정렬 결과가 여러 개인 경우
+        elif not certain:
+            print("?")
+
+        # 위상정렬을 수행한 결과 출력
+        else:
+            for i in ret:
+                print(i, end=' ')
+            print()
 
 
 if __name__ == "__main__":
     # travel_plan()
     # docking()
     # dark_road()
-    turnel()
+    # turnel()
+    final_rank()
