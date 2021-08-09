4fd1ba5f511637ae1b8615199c94de9769660860
ade <fowsdw@naver.com>
2021-08-09 23:09:41 +0900

    ade commit

--- programmers/practice.py
+++ programmers/practice.py
@@ -6,6 +6,7 @@
 '''
 
 from math import gcd
+import itertools
 
 '''
 # 최대공약수와 최소공배수
@@ -84,7 +85,104 @@ def pre_test(answers):
     return answer
 
 
+'''
+# [삼각 달팽이] 문제
+정수 n이 매개변수로 주어집니다. 다음 그림과 같이 밑변의 길이와 높이가 n인 삼각형에서 맨 위 꼭짓점부터 반시계 방향으로 달팽이 채우기를 진행한 후, 
+첫 행부터 마지막 행까지 모두 순서대로 합친 새로운 배열을 return 하도록 solution 함수를 완성해주세요.
+
+입출력 예
+n	result
+4	[1,2,9,3,10,8,4,5,6,7]
+5	[1,2,12,3,13,11,4,14,15,10,5,6,7,8,9]
+6	[1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11]
+
+Level 1부터 잘하자....
+'''
+
+
+def snail(n):
+    array = [[0 for x in range(y)] for y in range(1, n + 1)]
+    num = 1
+    x = -1
+    y = 0
+    test = n % 3
+
+    for i in range(n, 0, -1):
+        for j in range(i):
+            if n % 3 == test:
+                x = x + 1
+            elif n % 3 == (test - 1) or n % 3 == (test + 2):
+                y += 1
+            else:
+                x -= 1
+                y -= 1
+            array[x][y] = num
+            num += 1
+
+        n = n - 1
+    print("##########################")
+    print(list(itertools.chain(*array)))
+    print("##########################")
+
+    return list(itertools.chain(*array))
+
+
+'''
+유용한 getMax 함수
+'''
+
+
+def getMax(n):
+    answer = 0
+
+    if n == 1:
+        answer = 1
+    else:
+        answer = getMax(n - 1) + n
+
+    return answer
+
+
+'''
+개발자를 희망하는 죠르디가 카카오에 면접을 보러 왔습니다.
+
+코로나 바이러스 감염 예방을 위해 응시자들은 거리를 둬서 대기를 해야하는데 개발 직군 면접인 만큼
+아래와 같은 규칙으로 대기실에 거리를 두고 앉도록 안내하고 있습니다.
+
+대기실은 5개이며, 각 대기실은 5x5 크기입니다.
+거리두기를 위하여 응시자들 끼리는 맨해튼 거리1가 2 이하로 앉지 말아 주세요.
+단 응시자가 앉아있는 자리 사이가 파티션으로 막혀 있을 경우에는 허용합니다.
+
+places의 행 길이(대기실 개수) = 5
+places의 각 행은 하나의 대기실 구조를 나타냅니다.
+places의 열 길이(대기실 세로 길이) = 5
+places의 원소는 P,O,X로 이루어진 문자열입니다.
+places 원소의 길이(대기실 가로 길이) = 5
+P는 응시자가 앉아있는 자리를 의미합니다.
+O는 빈 테이블을 의미합니다.
+X는 파티션을 의미합니다.
+입력으로 주어지는 5개 대기실의 크기는 모두 5x5 입니다.
+return 값 형식
+1차원 정수 배열에 5개의 원소를 담아서 return 합니다.
+places에 담겨 있는 5개 대기실의 순서대로, 거리두기 준수 여부를 차례대로 배열에 담습니다.
+각 대기실 별로 모든 응시자가 거리두기를 지키고 있으면 1을, 한 명이라도 지키지 않고 있으면 0을 담습니다.
+
+places	result
+[["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], 
+["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
+["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], 
+["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
+["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]	
+[1, 0, 1, 1, 1]
+
+두 테이블 T1, T2가 행렬 (r1, c1), (r2, c2)에 각각 위치하고 있다면, 
+T1, T2 사이의 맨해튼 거리는 |r1 - r2| + |c1 - c2|
+
+'''
+
 if __name__ == "__main__":
     # print(solution(2, 5))
-    print(solution(["abce", "abcd", "cdx"],	2))
+    # print(solution(["abce", "abcd", "cdx"], 2))
     # pre_test([1,2,3,4,5])
+    # print(getMax(4))
+    snail(4)
