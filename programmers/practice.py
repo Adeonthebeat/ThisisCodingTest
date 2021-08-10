8c866e6a9d3dc6753818979ec0c74ec74e6b2bb8
ade <fowsdw@naver.com>
2021-08-10 23:17:41 +0900

    ade commit

--- programmers/practice.py
+++ programmers/practice.py
@@ -144,45 +144,48 @@ def getMax(n):
 
 
 '''
-개발자를 희망하는 죠르디가 카카오에 면접을 보러 왔습니다.
-
-코로나 바이러스 감염 예방을 위해 응시자들은 거리를 둬서 대기를 해야하는데 개발 직군 면접인 만큼
-아래와 같은 규칙으로 대기실에 거리를 두고 앉도록 안내하고 있습니다.
-
-대기실은 5개이며, 각 대기실은 5x5 크기입니다.
-거리두기를 위하여 응시자들 끼리는 맨해튼 거리1가 2 이하로 앉지 말아 주세요.
-단 응시자가 앉아있는 자리 사이가 파티션으로 막혀 있을 경우에는 허용합니다.
-
-places의 행 길이(대기실 개수) = 5
-places의 각 행은 하나의 대기실 구조를 나타냅니다.
-places의 열 길이(대기실 세로 길이) = 5
-places의 원소는 P,O,X로 이루어진 문자열입니다.
-places 원소의 길이(대기실 가로 길이) = 5
-P는 응시자가 앉아있는 자리를 의미합니다.
-O는 빈 테이블을 의미합니다.
-X는 파티션을 의미합니다.
-입력으로 주어지는 5개 대기실의 크기는 모두 5x5 입니다.
-return 값 형식
-1차원 정수 배열에 5개의 원소를 담아서 return 합니다.
-places에 담겨 있는 5개 대기실의 순서대로, 거리두기 준수 여부를 차례대로 배열에 담습니다.
-각 대기실 별로 모든 응시자가 거리두기를 지키고 있으면 1을, 한 명이라도 지키지 않고 있으면 0을 담습니다.
-
-places	result
-[["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], 
-["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
-["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], 
-["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
-["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]	
-[1, 0, 1, 1, 1]
-
-두 테이블 T1, T2가 행렬 (r1, c1), (r2, c2)에 각각 위치하고 있다면, 
-T1, T2 사이의 맨해튼 거리는 |r1 - r2| + |c1 - c2|
+주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 
+숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 
+소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
 
 '''
+
+
+def sosu(nums):
+    cnt = 0
+    for i in range(0, len(nums) - 2):
+        for j in range(i+1, len(nums) - 1):
+            for k in range(j+1, len(nums)):
+                if isPrimeNums(nums[i], nums[j], nums[k]):
+                    cnt += 1
+    return cnt
+
+def prime(nums):
+    cnt = 0
+    num_list = list(itertools.combinations(nums, 3))
+    for i in num_list:
+        if isPrimeNums(i[0], i[1], i[2]): cnt += 1
+    return cnt
+
+
+'''
+# 소수 판별
+'''
+def isPrimeNums(a, b, c):
+    tot = a + b + c
+    for i in range(2, tot):
+        if tot % i == 0:
+            return False
+    return True
+
+
 
 if __name__ == "__main__":
     # print(solution(2, 5))
     # print(solution(["abce", "abcd", "cdx"], 2))
     # pre_test([1,2,3,4,5])
     # print(getMax(4))
-    snail(4)
+    # snail(4)
+    # print(sosu([1,2,3,4]))
+    # print(prime([1,2,3,4]))
+    print(prime([1, 2, 7, 6, 4]))
