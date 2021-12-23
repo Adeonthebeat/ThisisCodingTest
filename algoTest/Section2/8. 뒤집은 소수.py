57d43f1d5677e9ca7c67b7573204b916fea4a3ef
ade <fowsdw@naver.com>
2021-12-24 06:45:44 +0900

    ade commit

--- algoTest/Section2/8. 뒤집은 소수.py
+++ algoTest/Section2/8. 뒤집은 소수.py
@@ -1,4 +1,3 @@
-
 # 8. 뒤집은 소수
 # 5
 # 32 55 62 3700 250
@@ -6,28 +5,28 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
+
 def reverse(x):
     res = 0
     while x > 0:
         t = x % 10
         res = res * 10 + t
-        x = x//10
+        x = x // 10
     return res
+
 
 def isPrime(x):
     if x == 1:
         return False
-    for i in range(2, x//2+1):
+    for i in range(2, x // 2 + 1):
         if x % i == 0:
             return False
     else:
         return True
 
 
-
-
 for x in arr:
     temp = reverse(x)
 
     if isPrime(temp):
-        print(temp, end= ' ')
+        print(temp, end=' ')
